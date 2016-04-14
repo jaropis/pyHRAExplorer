d004f93cd4d138c6b6fe1dea6b3e58f399b55c86
Jaroslaw Piskorski <jaropis@zg.home.pl>
2016-04-14 07:28:22 +0100

    added classic Poincare descriptors tests for one signal - this signal has only beats of sinus origin

--- signal_properties/poincare_tests.py
+++ signal_properties/poincare_tests.py
@@ -1,9 +1,28 @@
 import unittest
-from Poincare import Poincare
-from signal_properties.RRclasses import *
+from numpy import round
+from RRclasses import *
 
 
 class TestPoincareFiltering(unittest.TestCase):
 
     def setUp(self):
-        self.signal_real = Signal("RR.csv", 2, 3, annotation_filter = (2,))
+        self.signal_real1 = Signal("RR1.csv", 1, 2, annotation_filter=(2,))
+
+    def test_first_signal_SD1(self):
+        self.assertTrue(round(self.signal_real1.poincare.SD1, 3) == 36.807)  # this is an example from our tutorial
+        # uncomment the line below and comment the line above if you want to use the definition of variance which uses (n-1)
+        # self.assertTrue(round(self.signal_real1.poincare.SD1, 3) == 36.817)  # this is an example from our tutorial
+
+    def test_first_signal_SD2(self):
+        self.assertTrue(round(self.signal_real1.poincare.SD2, 3) == 86.253)  # this is an example from our tutorial
+        # uncomment the line below and comment the line above if you want to use the definition of variance which uses (n-1)
+        #self.assertTrue(round(self.signal_real1.poincare.SD2, 3) == 86.275)  # this is an example from our tutorial
+
+    def test_first_signal_SDNN(self):
+        # self.assertTrue(round(self.signal_real1.poincare.SDNN, 3) == 66.311)  # this is an example from our tutorial
+        # uncomment the line below and comment the line above if you want to use the definition of variance which uses (n-1)
+        self.assertTrue(round(self.signal_real1.poincare.SDNN, 3) == 66.328)  # this is an example from our tutorial
+
+
+if __name__ == '__main__':
+    unittest.main()
