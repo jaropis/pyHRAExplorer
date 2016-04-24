d88286d3db3d786cd5d626990e4d80c027db8eda
Jaroslaw Piskorski <jaropis@zg.home.pl>
2016-04-24 07:28:05 +0200

    separated filtering as a separate method of the Signal class - consequently, the same filtering will be used throughout the application

--- signal_properties/RRclasses.py
+++ signal_properties/RRclasses.py
@@ -1,5 +1,5 @@
 from re import findall
-from scipy import array
+from scipy import array, where
 from Poincare import Poincare
 from runs import Runs
 
@@ -8,32 +8,80 @@ from runs import Runs
 class Signal:
     def __init__(self, path_to_file, column_signal=0, column_annot=0, quotient_filter=-1, square_filter=(0, 8000), annotation_filter=()):
         # 0 are there to facilitate the construction of signals from console
-        self.signal, self.annotation = self.read_data(path_to_file, column_signal, column_annot)
         self.quotient_filter = quotient_filter
         self.square_filter = square_filter
         self.annotation_filter = annotation_filter
-        self.annotation_filter = annotation_filter
+        self.signal, self.annotation, self.sample_to_sample = self.read_data(path_to_file, column_signal, column_annot, 0)
+
+
+        # here the data is filtered - this filtration will apply throughout the whole application
+        self.filter_data()
+
+        # now the HRV and HRA methods are being applied
         self.poincare = Poincare(self)
         self.runs = Runs(self)
-        runs = Runs(self)
         # spectrum = Spectrum(self)
 
-    def read_data(self, path_to_file, column_signal, column_annot):
+    def read_data(self, path_to_file, column_signal, column_annot, column_sample_to_sample = -1):
         if type(path_to_file) == list:
             # this is the possibility to pass a list with signal and annotation vector as its elements
-            return array(path_to_file[0]), array(path_to_file[1])
+            return array(path_to_file[0]), array(path_to_file[1]), None
         reafile_current = open(path_to_file, 'r')
         reafile_current.readline()
         signal = []  # this variable contains the signal for spectral analysis
         annotation = []
+        sample_to_sample = [] # this variable holds the sample-to-sample values (like the beat-to-beat interval,
+        # RR interval) - this will be used in the Lomb-Scargle periodogram, which requires the time-track column
         # here the reading of the file starts
+
         for line in reafile_current:
             line_content = findall(r'\b[0-9\.]+', line)
             signal.append(float(line_content[column_signal-1]))
             if column_signal != column_annot:  # see below - similar condition
                 annotation.append(int(float(line_content[column_annot-1])))
+            if column_sample_to_sample != -1 and column_sample_to_sample != column_signal:
+                sample_to_sample.append(float(line_content[column_sample_to_sample-1]))
         signal = array(signal)
+
+        if column_sample_to_sample == column_signal:
+            sample_to_sample =column_signal
+
         if column_signal == column_annot:
             annotation = 0*signal
         annotation = array(annotation)
-        return signal, annotation
+        return signal, annotation, sample_to_sample
+
+    def filter_data(self):
+        """
+        this function defines the filter method. It uses the following parameters accepted by the constructor:
+        quotient - parameters of the quotient filter - the rejectance ratio - the initial value of -1 means "do not filter"
+        square - parameters of the square filter
+        annotation - parameters of the annotation filter - 1 means "remove from analysis" and refers to
+        (sinus, ventricular, supraventricular, artifact) respectively
+        """
+
+        # now, let the filtering begin
+        # beginning with the annotation filter
+        # 16 henceforth means "bad"
+        if len(self.annotation_filter)>0:
+            for beat_type in self.annotation_filter:
+                self.annotation[where(self.annotation == beat_type)] = 16
+
+        # now the square filter
+        self.annotation[where(self.signal < self.square_filter[0])[0]] = 16
+        self.annotation[where(self.signal > self.square_filter[1])[0]] = 16
+
+        # now removing bad beats from the beginning and the end of the recording
+        try:
+            while self.annotation[0] != 0:
+                self.signal = self.signal[1:]
+                self.annotation = self.annotation[1:]
+
+            # removing nonsinus beats from the end
+            while self.annotation[-1]!=0:
+                self.signal=self.signal[0:-1]
+                self.annotation=self.annotation[0:-1]
+        except IndexError:
+            print("no good beats")
+
+        return None
