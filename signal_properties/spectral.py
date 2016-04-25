6ff377b9b714fefc0ab363d04717b20a86a4daa3
Jaroslaw Piskorski <jaropis@zg.home.pl>
2016-04-25 13:01:04 +0200

    added Lomb-Scargle data preparation and tests of data preparation

--- signal_properties/spectral.py
+++ signal_properties/spectral.py
@@ -1,9 +1,19 @@
 from my_exceptions import WrongSignal
+import scipy
 
-
-class Runs:
+class LombScargleSpectrum:
     def __init__(self, signal):
-        self.filtered_signal, self.time_track = self.filter_and_timetrack(signal)
+        self.filtered_signal, self.filtered_time_track = self.filter_and_timetrack(signal)
 
     def filter_and_timetrack(self, signal):
-        pass
+        # this function prepares data for Lomb-Scargle - i.e. filtered cumulative sum of time,   filtered signal
+        bad_beats = scipy.where(signal.annotation != 0)[0]
+        filtered_timetrack = scipy.delete(signal.timetrack, bad_beats)
+        filtered_signal = scipy.delete(signal.signal, bad_beats)
+
+        return filtered_signal, filtered_timetrack
+
+
+
+
+
---
+++ signal_properties/spectral_tests.py
@@ -0,0 +1,31 @@
+import unittest
+from scipy import array
+from RRclasses import Signal
+from numpy import allclose
+from my_exceptions import WrongSignal
+
+# I learned something - each test calls setup
+
+class TestPoincareFiltering(unittest.TestCase):
+    def setUp(self):
+        self.signal1 = Signal([[1, 1, 1, 1], [0, 0, 0, 0, 0]])
+        self.signal2 = Signal([[1, 1, 1, 1, 1], [0, 0, 1, 0, 0]])
+        self.signal3 = Signal([[1, 2, 3, 4, 5, 6, 7, 8], [1, 0, 0, 1, 0, 0, 0, 1]])
+
+    def test_filter_and_timetrack(self):
+        # testing filtering and time tracking
+        self.assertTrue((self.signal1.LS_spectrum.filtered_signal == [1, 1, 1, 1]).all())
+        self.assertTrue((self.signal1.LS_spectrum.filtered_time_track == [1, 2, 3, 4]).all())
+
+        self.assertTrue((self.signal2.LS_spectrum.filtered_signal == [1, 1, 1, 1]).all())
+        self.assertTrue((self.signal2.LS_spectrum.filtered_time_track == [1, 2, 4, 5]).all())
+
+        #print(self.signal3.LS_spectrum.filtered_time_track)
+        #print(self.signal3.LS_spectrum.filtered_signal)
+        self.assertTrue((self.signal3.LS_spectrum.filtered_signal == [2, 3, 5, 6, 7]).all())
+        self.assertTrue((self.signal3.LS_spectrum.filtered_time_track == [3, 6, 15, 21, 28]).all())
+        #self.assertTrue((self.signal3.LS_spectrum.filtered_time_track == [1, 2, 4, 5]).all())
+
+
+if __name__ == '__main__':
+    unittest.main()
