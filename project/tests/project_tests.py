f1f92d5b43eca5ad4d29d6563ee44e99dffc32cb
Jaroslaw Piskorski <jaropis@zg.home.pl>
2016-05-12 23:55:36 +0200

    added real test for Poincare dump, also the tests now cleans up after itself

--- project/tests/project_tests.py
+++ project/tests/project_tests.py
@@ -9,6 +9,8 @@ class TestProject(unittest.TestCase):
         self.test_project.set_runs()
         self.test_project.set_LS_spectrum()
         self.test_project.step_through_project_files()
+        self.test_project2 = Project(path=os.getcwd()+"/test_files2", file_extension=".rea", column_signal=1, column_annot=2, column_sample_to_sample=1)
+        self.test_project2.set_runs()
 
     def test_files_list(self):
         self.test_project.get_files_list()
@@ -18,20 +20,54 @@ class TestProject(unittest.TestCase):
     def test_project_runs(self):
         self.assertTrue(True)
 
-    # def test_write_state(self):
-    #     self.assertTrue(self.test_project.write_state())
-    #     temp = self.test_project.path
-    #     self.test_project.path = "/"
-    #     self.assertFalse(self.test_project.write_state())
-    #     self.test_project.path = temp
+    def test_write_state(self):
+        self.assertTrue(self.test_project.write_state())
+        temp = self.test_project.path
+        self.test_project.path = "/"
+        self.assertFalse(self.test_project.write_state())
+        self.test_project.path = temp
 
-    # def test_read_state(self):
-    #     self.test_write_state()
-    #     self.assertTrue(self.test_project.read_state())
-    #     self.assertEqual(self.test_project.Poincare_state, 0)
+    def test_read_state(self):
+        self.test_write_state()
+        self.assertTrue(self.test_project.read_state())
+        self.assertEqual(self.test_project.Poincare_state, 1)
 
-    def test_wdump_Poincare(self):
+    def test_dump_Poincare(self):
+        """
+        dumps the Poincare results for the test files, tests if the dumped file exists and cleans up
+        :return:
+        """
+        import os
         self.test_project.dump_Poincare()
+        local_path = self.test_project.build_name(prefix="PPoincare_") # double P so as to fool the build_name method
+        # into NOT adding _1 to the name
+        local_path = local_path.replace("PPoincare", "Poincare")
+        self.assertTrue(os.path.exists(local_path))
+        os.remove(local_path)
+
+    def test_build_name(self):
+        """
+        this is a pseudo test - I just want to know if it fails
+        ;return:
+        """
+        self.test_project.build_name()
         self.assertTrue(True)
+
+    # def test_dump_Poincare(self):
+    #     """
+    #     this is a pseudo test - I just want to know if it fails
+    #     :return:
+    #     """
+    #     self.test_project.dump_Poincare()
+    #     self.assertTrue(True)
+
+    # def test_longest_runs(self):
+    #     """
+    #
+    #     :return:
+    #     """
+    #     print(self.test_project2.find_longest_runs())
+    #     self.assertTrue(True)
+
 if __name__ == '__main__':
     unittest.main()
