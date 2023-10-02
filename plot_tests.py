819f38335b57ed2099a786911af192aad5363523
maria-guzik <81884083+maria-guzik@users.noreply.github.com>
2023-10-02 15:11:28 +0100

    Parameterized pnnx, cleaned testing file
    pNNx can now both be accessed as a pNN50 attribute or as as poincare.pnnx(x) method. All functions within the Poincare class should be refered to as methods (will fix RRclasses next)

--- plot_tests.py
+++ plot_tests.py
@@ -1,56 +1,8 @@
 from signal_properties.RRclasses import Signal
 import matplotlib.pyplot as plt
-# Created to test the new plot function
-
-# PLACE TO TEST PLOTS WILL BE REMOVED (code moved to the plotRR.py)
+# PLACE FOR TESTS - WILL BE REMOVED 
 
 rr = Signal("C:/Users/k52672mg/OneDrive - The University of Manchester/Analysis/PIPS_HRAEXPLORER/test_files/0582a.rea", 1, 2, 0, square_filter=(500,1500), annotation_filter=(1, 2, 3))
 rr.set_poincare()
-global_min = min([min(rr.poincare.xii[1:]), min(rr.poincare.xi[1:])])
-#print(rr.poincare.xi)
-
-#fig, (RRplot, RRhist) = plt.subplots(nrows=2, sharex= True, subplot_kw=dict(frameon=False))
-#RRplot = plt.subplot(3,2,(1,4))
-#RRplot = plt.subplot(2,1,1)
-#RRplot.scatter(x = rr.poincare.xi[1:], y = rr.poincare.xii[1:],  facecolors='none', edgecolors= 'darkblue', s = 30, alpha = 0.5)
-#RRplot.axline((global_min-50,global_min-50), slope=1,c = 'black', ls = '--')
-#RRplot.set_xlabel('Rnn')
-#RRplot.set_ylabel('RRn+1')
-#RRplot.set(adjustable='box', aspect='equal')
-#RRplot.axis('square')
-#print(rr.poincare.xii[1:])
-#print(rr.poincare.xi[1:])
-#print(rr.poincare.filtered_time[1:])
-#RRhist = plt.subplot(3,2,(5,6))
-#RRhist = plt.subplot(2,1,2)
-#RRhist.hist(rr.poincare.xi[1:], bins = 25, edgecolor = 'darkblue')
-#RRhist.axvline(x = rr.poincare.meanRR, linestyle = 'dashed', color = 'red', linewidth = 2)
-#RRhist.axvline(x = rr.poincare.meanRR + rr.poincare.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1.5)
-#RRhist.axvline(x = rr.poincare.meanRR - rr.poincare.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1.5)
-#RRhist.set_ylabel('Count')
-#RRhist.set_xlabel('Rnn')
-
-#print(len(rr.timetrack), len(rr.signal), len(rr.poincare.xii))
-#print(where(rr.annotation == 0)[0])
-#print(rr.timetrack)
-#print(rr.poincare.xi, rr.poincare.xii, rr.poincare.filtered_time)
-
-#plt.plot(rr.poincare.filtered_time, rr.poincare.xii, color='black', linestyle='--', linewidth=0.3)
-#plt.scatter(x = rr.poincare.filtered_time, y = rr.poincare.xii,  facecolors='none', edgecolors= 'black', s = 10)
-#plt.axhline(y = rr.poincare.meanRR, linestyle = 'dashed', color = 'red', linewidth = 1)
-#plt.axhline(y = rr.poincare.meanRR + rr.poincare.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1)
-#plt.axhline(y = rr.poincare.meanRR - rr.poincare.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1)
-#plt.show()
-
-
-#print('meanRR:', rr.poincare.meanRR)
-#print('CV:', rr.poincare.CV)
-#plt.show()
-#rr.plotRR.PPplot
-
-#print(rr.annotation)
-
-
-#print(rr.poincare.HRA1, rr.poincare.HRA2, rr.poincare.HRAT, rr.poincare.HRAN, rr.poincare.HRAcomp)
-#print(rr.poincare.SDNN, rr.poincare.SD1, rr.poincare.SD1, rr.poincare.SD2I, rr.poincare.meanRR, rr.poincare.SDNNd, rr.poincare.SDNNa, rr.poincare.SD1d, rr.poincare.SD1a, rr.poincare.SD2a, rr.poincare.ND)
-print(rr.annotation, rr.annotation_filter)
+print(rr.poincare.pNN50, rr.poincare.pnnx(), rr.poincare.pnnx(100))
+#14.316820903647251 14.316820903647251 0.43549265106151336
