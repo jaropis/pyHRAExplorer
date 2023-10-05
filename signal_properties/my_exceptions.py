80ae6d65dea5240dadcbbc974ac6075716cd25ea
maria-guzik <81884083+maria-guzik@users.noreply.github.com>
2023-10-05 15:46:41 +0100

    WrongSignal and WrongCuts Exceptions documentation

--- signal_properties/my_exceptions.py
+++ signal_properties/my_exceptions.py
@@ -1,6 +1,12 @@
 class WrongSignal(Exception):
-    # this exception should be raised if the signal for runs calculations is shorter than 2
+    '''
+    Exception raised if the signal for runs calculations is shorter than 2
+    '''
     pass
 
 class WrongCuts(Exception):
+    '''
+    Exception raised if the cuts for finsing power in bands in the spectral analysis are not unique or 
+    are not arranged in an increasing order.
+    '''
     pass
