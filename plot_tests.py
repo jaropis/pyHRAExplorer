b40fbada76331be28581c804081d1691fddeb54e
maria-guzik <81884083+maria-guzik@users.noreply.github.com>
2023-10-09 16:29:11 +0100

    PlotRR class and p_plot method documentation
    > Changed the way parameters are called to fix the previous issues

--- plot_tests.py
+++ plot_tests.py
@@ -7,8 +7,11 @@ import scipy.signal as sc
 rr = Signal("C:/Users/k52672mg/OneDrive - The University of Manchester/Analysis/PIPS_HRAEXPLORER/test_files/0582a.rea", 1, 2, 0, square_filter=(500,1500), annotation_filter=(1, 2, 3))
 rr.set_poincare()
 rr.set_LS_spectrum()
+rr.set_plots()
 print(rr.poincare.pNN50, rr.poincare.pnnx(), rr.poincare.pnnx(100))
 #14.316820903647251 14.316820903647251 0.43549265106151336
 
-rr.LS_spectrum.plot_periodogram(mode = 'Hz', color = 'red', xlim = (0, 0.2))
+#rr.LS_spectrum.plot_periodogram(mode = 'Hz', color = 'red', xlim = (0, 0.2))
+
+rr.plotRR.pplot()
 plt.show()
--- signal_properties/plotRR.py
+++ signal_properties/plotRR.py
@@ -8,22 +8,46 @@ import matplotlib.pyplot as plt
 # WORK IN PROGRESS SOME PLOTS ARE MISSING LEGENDS/AXIS NAMES
 
 class PlotRR:
-    def __init__(self, signal):
-       self.PPLOT =  self.pplot(signal)
-       self.RR_HISTOGRAM = self.rr_histogram(signal)
-       self.TACHOGRAM = self.tachogram(signal)
+    '''
+    Class for creating plots based on the signal and timetrack parameters
 
-    def pplot(self, signal, color = 'darkblue', size = 30):
+    Attributes:
+        xi (array): An array containting the filtered RRn signal values
+        xii (array): An array containting the filtered RRn+1 signal values
+        filtered_time (array): An array containting the filtered values of timetrack (corresponding to xi)
+        meanRR (float): Stores the value of mean RR signal after filtering
+        SDNN (float): Stores the value of the square root of the total RR intervals variance
+    '''
+    def __init__(self, signal):
         '''
-        This function uses xi and xii created by the prepare_PP function (in Poincare.py) to build a Poincare plot.
-        In the Poncare plot, Rnn (xi) is plotted against Rnn+1 (xii). Alongside the the points, the plot also includes
+        Initializes the attributes of the plotRR class, creates objexts needed for the plots.
+
+        Args:
+            signal (Signal): Object of class signal, its filtered and signal and timetrack properties will be used for plots
+        '''
+        #self.PPLOT =  self.pplot(signal)
+        #self.RR_HISTOGRAM = self.rr_histogram(signal)
+        #self.TACHOGRAM = self.tachogram(signal)
+        self.xi, self.xii = signal.poincare.prepare_PP(signal)
+        self.filtered_time = signal.poincare.filter_time(signal)
+        self.meanRR = signal.poincare.meanRR
+        self.SDNN = signal.poincare.SDNN
+
+    def pplot(self, color = 'darkblue', size = 30):
+        '''
+        This method builds a Poincare plot, whrer Rnn (xi) is plotted against Rnn+1 (xii). Alongside the the points, the plot also includes
         a dashed identity line (x = y).
-        The function accepts color (string) and size (int) parameters to change the colour and size of the points
-        on the Poincare plot respectively. 
+
+        Args:
+            color (string): Astring that indicates the desired colour of the points, dark blue by default.
+            size (int): Integer that determines the size of the points, 30 by default
+
+        Returns:
+            p_plot (pyplot): The completed plot 
         '''
-        global_min = min([min(signal.poincare.xii[1:]), min(signal.poincare.xi[1:])])
+        global_min = min([min(self.xii[1:]), min(self.xi[1:])])
         p_plot = plt.figure()
-        plt.scatter(x = signal.poincare.xi[1:], y = signal.poincare.xii[1:],  facecolors='none', edgecolors= color, s = size, alpha = 0.5)
+        plt.scatter(x = self.xi[1:], y = self.xii[1:],  facecolors='none', edgecolors= color, s = size, alpha = 0.5)
         plt.axline((global_min-50,global_min-50), slope=1,c = 'black', ls = '--')
         plt.xlabel('Rnn')
         plt.ylabel('RRn+1')
@@ -32,7 +56,7 @@ class PlotRR:
         #plt.show()
         return p_plot
     
-    def rr_histogram(self, signal, color = 'blue', edgecolor = 'darkblue', bins = 25):
+    def rr_histogram(self, color = 'blue', edgecolor = 'darkblue', bins = 25):
         '''
         This function uses xi created from prepare_PP function to plot the distribution of RR signals as a histogram.
         Alongside the histogram, mean and mean +/- sd are shown as red and green lines respectively.
@@ -40,16 +64,16 @@ class PlotRR:
         number. 
         '''
         hist = plt.figure()
-        plt.hist(signal.poincare.xi[1:], bins = bins, edgecolor = edgecolor, color = color)
-        plt.axvline(x = signal.poincare.meanRR, linestyle = 'dashed', color = 'red', linewidth = 2)
-        plt.axvline(x = signal.poincare.meanRR + signal.poincare.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1.5)
-        plt.axvline(x = signal.poincare.meanRR - signal.poincare.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1.5)
+        plt.hist(self.xi[1:], bins = bins, edgecolor = edgecolor, color = color)
+        plt.axvline(x = self.meanRR, linestyle = 'dashed', color = 'red', linewidth = 2)
+        plt.axvline(x = self.meanRR + self.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1.5)
+        plt.axvline(x = self.meanRR - self.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1.5)
         plt.ylabel('Count')
         plt.xlabel('Rnn')
         #plt.show()
         return hist
     
-    def tachogram(self, signal, markersize = 5, linewidth = 0.3, color = 'black'):
+    def tachogram(self, markersize = 5, linewidth = 0.3, color = 'black'):
         '''
         Function that uses the xii and filtered_time to create a tachogram, ploting RRsignal against time. 
         Together with the tachogram, the mean and mean +/- sd of the RR signal are also displayed as red and green 
@@ -58,11 +82,11 @@ class PlotRR:
         edge thickness and colour of the ploted points. 
         '''
         tacho = plt.figure()
-        plt.plot(signal.poincare.filtered_time, signal.poincare.xii, color=color, linestyle='--', linewidth=linewidth)
-        plt.scatter(x = signal.poincare.filtered_time, y = signal.poincare.xii,  facecolors='none', edgecolors= color, alpha = 0.75, s = markersize, linewidths = 0.5)
-        plt.axhline(y = signal.poincare.meanRR, linestyle = 'dashed', color = 'red', linewidth = 1)
-        plt.axhline(y = signal.poincare.meanRR + signal.poincare.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1)
-        plt.axhline(y = signal.poincare.meanRR - signal.poincare.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1)
+        plt.plot(self.filtered_time, self.xii, color=color, linestyle='--', linewidth=linewidth)
+        plt.scatter(x = self.filtered_time, y = self.xii,  facecolors='none', edgecolors= color, alpha = 0.75, s = markersize, linewidths = 0.5)
+        plt.axhline(y = self.meanRR, linestyle = 'dashed', color = 'red', linewidth = 1)
+        plt.axhline(y = self.meanRR + self.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1)
+        plt.axhline(y = self.meanRR - self.SDNN, linestyle = 'dashed', color = 'lightgreen', linewidth = 1)
         plt.ylabel('RR')
         plt.xlabel('Time [min]')
         #plt.show()
